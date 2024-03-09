055bf7667a1c4547a2f7edbe7d693f54f1678145
rajeshray <iamrajesh350@gmail.com>
2024-03-09 15:01:21 +0530

    added icon change logic

--- app/src/main/java/com/ceresdroidxapps/taskapp/utils/Extensions.kt
+++ app/src/main/java/com/ceresdroidxapps/taskapp/utils/Extensions.kt
@@ -1,8 +1,14 @@
 package com.ceresdroidxapps.taskapp.utils
 
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 
 fun View.show() {
     this.visibility = View.VISIBLE
@@ -17,5 +23,62 @@ inline fun <reified T> Intent.getParcelableModel(key: String): T? {
         this.getParcelableExtra(key, T::class.java)
     } else {
         this.getParcelableExtra(key)
+    }
+}
+
+
+fun Activity.changeIconToMain(context: Context) {
+    try {
+        // Get the package manager instance
+        val packageManager = context.packageManager
+
+        val secondIconComponent = ComponentName(context, "com.ceresdroidxapps.taskapp.SecondIcon")
+        packageManager.setComponentEnabledSetting(
+            secondIconComponent,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+
+        // Enable the main activity component
+        val defaultIcon = ComponentName(context, "com.ceresdroidxapps.taskapp.DefaultIcon")
+        packageManager.setComponentEnabledSetting(
+            defaultIcon,
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+
+        // Log and display a toast message indicating that the main logo is enabled
+        Toast.makeText(context.applicationContext, "Main Logo Enabled.", Toast.LENGTH_SHORT)
+            .show()
+    } catch (e: java.lang.Exception) {
+        Log.e("Exception", e.toString())
+    }
+}
+
+fun Activity.changeToSecondIcon(context: Context) {
+    try {
+        // Get the package manager instance
+        val packageManager = context.packageManager
+
+        // Enable the main activity component
+        val defaultIcon = ComponentName(context, "com.ceresdroidxapps.taskapp.DefaultIcon")
+        packageManager.setComponentEnabledSetting(
+            defaultIcon,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+
+        val secondIconComponent = ComponentName(context, "com.ceresdroidxapps.taskapp.SecondIcon")
+        packageManager.setComponentEnabledSetting(
+            secondIconComponent,
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+
+        // Log and display a toast message indicating that the main logo is enabled
+        Toast.makeText(context.applicationContext, "Second Logo Enabled.", Toast.LENGTH_SHORT)
+            .show()
+    } catch (e: java.lang.Exception) {
+        Log.e("Exception", e.toString())
     }
 }
--- app/src/main/java/com/ceresdroidxapps/taskapp/views/MainActivity.kt
+++ app/src/main/java/com/ceresdroidxapps/taskapp/views/MainActivity.kt
@@ -1,35 +1,29 @@
 package com.ceresdroidxapps.taskapp.views
 
-import androidx.activity.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.ceresdroidxapps.taskapp.R
-import com.ceresdroidxapps.taskapp.adapters.MovieItemClickListener
-import com.ceresdroidxapps.taskapp.adapters.MovieListAdapter
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
 import com.ceresdroidxapps.taskapp.base.BaseActivity
-import com.ceresdroidxapps.taskapp.data.model.MovieItem
-import com.ceresdroidxapps.taskapp.data.network.Resource
-import com.ceresdroidxapps.taskapp.data.network.Status
 import com.ceresdroidxapps.taskapp.databinding.ActivityMainBinding
-import com.ceresdroidxapps.taskapp.utils.PagingScrollListener
-import com.ceresdroidxapps.taskapp.utils.hide
-import com.ceresdroidxapps.taskapp.utils.show
-import com.ceresdroidxapps.taskapp.viewmodels.MainViewModel
+import com.ceresdroidxapps.taskapp.utils.changeIconToMain
+import com.ceresdroidxapps.taskapp.utils.changeToSecondIcon
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate),
-    MovieItemClickListener {
-
-    private val viewModel: MainViewModel by viewModels()
-
-    private var isLoading = false
-    private var movieListAdapter: MovieListAdapter? = null
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     override fun initUI() {
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = "Movie list"
-        setUpAdapter()
+
+        binding.secondIconButton.setOnClickListener {
+            changeToSecondIcon(this@MainActivity)
+        }
+        binding.mainIconButton.setOnClickListener {
+            changeIconToMain(this@MainActivity)
+        }
     }
 
     override fun initListeners() {
@@ -37,92 +31,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     override fun initObservers() {
-        viewModel.apply {
-            movieListLiveData.observe(this@MainActivity) { list ->
 
-                if (!movieList.isNullOrEmpty()) {
-                    isLoading = false
-                    movieListAdapter?.removeLoadingView()
-                }
-
-                if (list != null) {
-                    setUpView(list)
-                }
-            }
-        }
-    }
-
-    private fun setUpView(resource: Resource<List<MovieItem>>) {
-        when (resource.status) {
-            Status.ERROR -> {
-                binding.lottie.setAnimation(R.raw.bot_error)
-                binding.lottie.playAnimation()
-                binding.loader.hide()
-                binding.rv.hide()
-            }
-
-            Status.SUCCESS -> {
-                binding.loader.hide()
-                binding.rv.show()
-                addDataToMovieList(resource.data)
-            }
-
-            Status.LOADING -> {
-                binding.loader.show()
-                binding.rv.hide()
-            }
-        }
-    }
-
-    private fun addDataToMovieList(data: List<MovieItem>?) {
-       if (!data.isNullOrEmpty()) {
-           movieListAdapter?.addAll(data)
-       }
-    }
-
-
-    private fun setUpAdapter() {
-
-        movieListAdapter = MovieListAdapter(
-            this,
-            viewModel.movieList,
-            this
-        )
-
-        val layoutManager = LinearLayoutManager(this)
-
-
-        val scrollListener = object : PagingScrollListener(layoutManager) {
-
-            override fun isLoading(): Boolean {
-                return isLoading
-            }
-
-            override fun loadMoreItems(totalItems: Int) {
-
-                if (totalItems < viewModel.totalMovies) {
-                    isLoading = true
-                    movieListAdapter?.addLoadingView()
-                    viewModel.apply {
-                        currentPage++
-                        getAllMovies()
-                    }
-                }
-            }
-        }
-
-
-        binding.rv.apply {
-            this.adapter = movieListAdapter
-            this.layoutManager = layoutManager
-            addOnScrollListener(scrollListener)
-        }
-    }
-
-    override fun onClickItem(item: MovieItem?) {
-        item?.let {
-            val detailsActivityIntent = DetailsActivity.createIntent(this, it)
-            startActivity(detailsActivityIntent)
-        }
     }
 }
